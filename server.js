5c9de540b9397a4d4bb2893049c677b57d8bc27f
Zach Leone <zjleone@gmail.com>
2023-04-01 14:22:31 -0700

    initial file structure and schema build

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: 'whitesox',
+    database: 'movies_db'
+  },
+  console.log(`Connected to the movies_db database.`)
+);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+  
